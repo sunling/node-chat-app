dd217b940404940e7ec13d650f99a5c1accc48ab
Ling Sun <lingsun@mum.edu>
2018-07-28 22:51:55 -0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const publicPath = path.join(__dirname,'../public');
+
+var app =express();
+const port = process.env.PORT || 3000;
+
+console.log(__dirname+"/../public");
+console.log(publicPath);
+
+app.use(express.static(publicPath));
+app.listen(port,()=>{
+    console.log("Server is up on port ",port);
+});
